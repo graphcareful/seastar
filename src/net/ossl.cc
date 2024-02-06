aadbd8fdd347747ee3108d4af5544a291f092c62
Rob Blafford <rob@redpanda.com>
2024-02-06 14:30:17 -0500

    build: Conditionally build with openssl
    - Also disables websockets

---
+++ src/net/ossl.cc
@@ -0,0 +1,62 @@
+/*
+ * This file is open source software, licensed to you under the terms
+ * of the Apache License, Version 2.0 (the "License").  See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership.  You may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+/*
+ * Copyright 2015 Cloudius Systems
+ */
+
+#ifdef SEASTAR_MODULE
+module;
+#endif
+
+#include <system_error>
+#include <openssl/err.h>
+
+#ifdef SEASTAR_MODULE
+module seastar;
+#else
+#include <seastar/net/tls.hh>
+#include <seastar/core/sstring.hh>
+#endif
+
+namespace seastar {
+
+class ossl_error : public std::runtime_error {
+public:
+    ossl_error() : std::runtime_error(build_error()) {}
+
+    ossl_error(const sstring& msg)
+      : std::runtime_error(format("{}: {}", msg, build_error())) {}
+
+    ossl_error(int ec, const sstring& msg)
+      : std::runtime_error(format("Error code: {}, msg: {}",  ec, build_error())) {}
+
+private:
+    static sstring build_error(){
+        sstring msg = "{";
+        std::array<char, 256> buf{};
+        for (auto code = ERR_get_error(); code != 0; code = ERR_get_error()) {
+            ERR_error_string_n(code, buf.data(), buf.size());
+            msg += fmt::format("{{{}: {}}}", code, buf.data());
+        }
+        msg += "}";
+
+        return msg;
+    }
+};
+
+}
